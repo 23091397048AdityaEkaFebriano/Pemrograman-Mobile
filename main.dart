1dcccb03d50331599646a9a648d181a39382d080
YogaAriAnggoro <148923210+YogaAriAnggoro@users.noreply.github.com>
2025-03-01 20:52:59 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,3 +6,11 @@ void main() {
   runApp(const MyApp()); // Menjalankan aplikasi dengan widget MyApp sebagai root widget.
 }
 
+// Definisi kelas `MyApp` yang merupakan StatelessWidget (widget yang tidak memiliki state).
+class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // Constructor untuk widget MyApp, menggunakan key super untuk identifikasi.
+
+  @override
+  Widget build(BuildContext context) {
+    // Fungsi build yang mendefinisikan UI dari widget ini.
+    return MaterialApp(  

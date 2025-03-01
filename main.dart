69dcbe456b6fe9f0bdace477c145358dc85c4510
Aditya Eka Febriano <149566858+23091397048AdityaEkaFebriano@users.noreply.github.com>
2025-03-01 19:47:00 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart'; // Import library Flutter untuk menggunakan Material design.
+import 'home_page_widget.dart'; // Mengimport file 'home_page_widget.dart', pastikan file ini ada di proyek.
+
+// Fungsi utama yang dipanggil pertama kali ketika aplikasi Flutter berjalan.
+void main() {
+  runApp(const MyApp()); // Menjalankan aplikasi dengan widget MyApp sebagai root widget.
+}
 

6dfbf7d785ad2089356244aab687af831133f759
YogaAriAnggoro <148923210+YogaAriAnggoro@users.noreply.github.com>
2025-03-01 20:54:54 +0700

    Update home_page_widget.dart

--- home_page_widget.dart
+++ home_page_widget.dart
@@ -39,3 +39,43 @@ class _HomePageWidgetState extends State<HomePageWidget> {
             ),
           ),
           elevation: 2, // Memberikan efek bayangan dengan elevasi 2.
+          ),
+        body: SafeArea(
+          // Membungkus body dengan SafeArea agar isi tampilan tidak terhalang oleh status bar atau notch.
+          child: Column(
+            mainAxisSize: MainAxisSize.max, // Kolom menggunakan maksimal ruang yang tersedia.
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 50), // Jarak atas dari gambar pertama sebesar 50.
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(22), // Membuat gambar memiliki border dengan sudut melengkung.
+                  child: Image.asset(
+                    'assets/images/permata.png', // Menampilkan gambar dari asset lokal.
+                    width: 200, // Lebar gambar 200.
+                    height: 200, // Tinggi gambar 200.
+                    fit: BoxFit.cover, // Gambar memenuhi area sesuai ukuran widget tanpa terdistorsi.
+                  ),
+                ),
+              ),
+              // Memanggil fungsi `buildQuoteContainer` untuk menampilkan widget quote dengan gambar dan teks.
+              buildQuoteContainer(
+                'assets/images/semangat.png', // Gambar motivasi 1.
+                "Keep the spirit even though your life is just like that.\n\n~Aditya Eka Febriano~", // Quote dari Aditya Eka Febriano.
+                Colors.blue, // Warna background container.
+              ),
+              buildQuoteContainer(
+                'assets/images/berhasil.png', // Gambar motivasi 2.
+                "Success is at the end of the story.\n\n~Yoga Ari Anggoro~", // Quote dari Yoga Ari Anggoro.
+                Colors.green, // Warna background container.
+              ),
+              buildQuoteContainer(
+                'assets/images/gagal.png', // Gambar motivasi 3.
+                "The biggest mistake is not failure, but stopping and giving up before tasting success.\n\n~Wisnu Prabowo~", // Quote dari Wisnu Prabowo.
+                Colors.orange, // Warna background container.
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
